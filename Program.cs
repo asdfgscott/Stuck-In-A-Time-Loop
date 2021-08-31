e7e866da7f180d093bd44a29de5e65caeb2f01b7
Scott Truong <asdfgscott@gmail.com>
2021-08-31 16:55:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stuck_In_A_Time_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(i +" Abracadabra");
+            }
+            
+        }
+    }
+}
